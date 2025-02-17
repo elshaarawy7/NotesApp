0e6a06c6214df21b5e8a5b8fc753ca519f9cbf01
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-17 14:38:11 +0200

    create nots view body

---
+++ lib/widgets/nots_views_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NotsViewsBody extends StatelessWidget {
+  const NotsViewsBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

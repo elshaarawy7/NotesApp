cdadcf6cf8aa36ecda5aa3a0c60bc5fe370efd6a
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-17 14:33:24 +0200

    create nots view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: Scaffold(),
+      home:const NotesView()
     );
   }
 }

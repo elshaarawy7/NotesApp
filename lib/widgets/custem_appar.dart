7334f4cedba8cdcdb6ee6cdbee75174535c422ba
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-17 14:57:22 +0200

    create custem appBar

---
+++ lib/widgets/custem_appar.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custem_search_icon.dart';
+
+class CustemAppar extends StatelessWidget {
+  const CustemAppar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24 , vertical: 50),
+      child: Row(
+        children: const [
+          Text("Notes" , style: TextStyle(
+            color: Colors.white ,
+            fontSize: 25,
+            fontWeight: FontWeight.bold ,
+          ),) ,
+
+          SizedBox(
+            width:double.infinity,
+            ),
+
+          CustemSearchIcon()
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/nots_views_body.dart
+++ lib/widgets/nots_views_body.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custem_appar.dart';
 
 class NotsViewsBody extends StatelessWidget {
   const NotsViewsBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      children: [
+        CustemAppar()
+      ],
+    );
   }
 }

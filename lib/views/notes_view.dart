8b8605337d3db3731680ea2536fb91559dcb4f98
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-17 20:34:08 +0200

    create wudgets custem textfield

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -13,6 +13,9 @@ class NotesView extends StatelessWidget {
     
         onPressed: () {
           showModalBottomSheet(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
             context: context, 
             builder: (context){
               return AddBatomSheate();
---
+++ lib/widgets/custem_textField.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustemTextfield extends StatelessWidget {
+  const CustemTextfield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

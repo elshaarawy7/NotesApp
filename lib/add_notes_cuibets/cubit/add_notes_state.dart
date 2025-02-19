d1e64b574117e3360e264ccc41abada17f2ee983
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-19 14:36:10 +0200

    add notes state

--- lib/add_notes_cuibets/cubit/add_notes_state.dart
+++ lib/add_notes_cuibets/cubit/add_notes_state.dart
@@ -3,4 +3,13 @@ part of 'add_notes_cubit.dart';
 @immutable
 sealed class AddNotesState {}
 
-final class AddNotesInitial extends AddNotesState {}
+ class AddNotesInitial extends AddNotesState {}
+ class AddNotesLoading extends AddNotesState {}
+ class AddNotesSuccess extends AddNotesState {}
+ class AddNotesFailur extends AddNotesState {
+  final String errMessage ;
+
+  AddNotesFailur(this.errMessage);
+ }
+
+

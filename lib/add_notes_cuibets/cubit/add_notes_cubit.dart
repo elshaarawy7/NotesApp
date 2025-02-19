130d3ac196f581d955372b5442056e717c48ee80
ELshaarawy hassan <elshaarawyhassan7@gmail.com>
2025-02-19 14:54:15 +0200

    add mode method add cubits

--- lib/add_notes_cuibets/cubit/add_notes_cubit.dart
+++ lib/add_notes_cuibets/cubit/add_notes_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/const.dart';
+import 'package:notes_app/model/note_modal.dart';
 
 part 'add_notes_state.dart';
 
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddNotesInitial());
+
+  addNote(NoteModal note) async {
+    emit(AddNotesLoading());
+    try {
+      var noetsBox = Hive.box(KNotesBocx);
+      emit(AddNotesSuccess());
+      await noetsBox.add(note);
+    } catch (e) {
+      AddNotesFailur(e.toString());
+    }
+  }
 }
